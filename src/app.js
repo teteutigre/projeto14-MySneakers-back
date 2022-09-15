b3803399e8b7cd0b118d73d211cee4eda1e52ff5
mateus <mateus.tigreewallet@gmail.com>
2022-09-15 16:07:58 -0300

    chore: adjust code for deploy on heroku

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRouter from "./routers/authRouter.js";
-import { MongoClient } from "mongodb";
 
 dotenv.config();
 const app = express();
@@ -11,6 +10,6 @@ app.use(express.json());
 
 app.use(authRouter);
 
-const mongoClient = new MongoClient(process.env.MONGO_URI);
-const PORT = process.env.PORT;
-app.listen(PORT || 5000, () => console.log(`Listening on port ${PORT}`));
+app.listen(process.env.PORT, () => {
+  console.log("Server running on port " + process.env.PORT);
+});
